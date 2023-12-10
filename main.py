b99612decd18a8ac770601be222864597a19d2bb
Shivam Agarwal <agshiv92@gmail.com>
2023-12-10 16:22:25 +0800

    Data ingestion Completed

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from src.Chicken_Diesease_Classification import logger
+from src.Chicken_Diesease_Classification.pipeline.stage_01_data_ingestion import  DataIngestionTrainingPipeline
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<<<<\n\n x===========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
