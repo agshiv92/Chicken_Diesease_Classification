12d9dfee7c2eb78fd214292f9169525c50d4eda1
Shivam Agarwal <agshiv92@gmail.com>
2023-12-17 13:11:45 +0800

    Moedel training  Completed

---
+++ src/Chicken_Diesease_Classification/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,28 @@
+from src.Chicken_Diesease_Classification.config.configuration import ConfigurationManager
+from src.Chicken_Diesease_Classification.components.model_trainer import ModelTrainer
+from src.Chicken_Diesease_Classification import logger
+
+
+
+STAGE_NAME = "Model Trainer stage"
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config = model_trainer_config)
+        model_trainer_config.train()
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
