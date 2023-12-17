8460fbab0021a1bba54d69b3e2c5605c54096788
Shivam Agarwal <agshiv92@gmail.com>
2023-12-17 13:11:45 +0800

    Moedel training  Completed

--- README.md
+++ README.md
@@ -12,3 +12,23 @@
 8. Update the main.py
 9. Update the app.py
 9. Update the dvc.yaml
+
+
+[dagshub](https://dagshub.com/dashboard)
+MLFLOW_TRACKING_URI=https://dagshub.com/agshiv92/Chicken_Diesease_Classification.mlflow \
+MLFLOW_TRACKING_USERNAME=agshiv92 \
+MLFLOW_TRACKING_PASSWORD=1d32aaac3b8893b6efd8712ee33e31467dde3aa8 \
+python script.py
+
+
+'''bash
+export MLFLOW_TRACKING_URI=https://dagshub.com/agshiv92/Chicken_Diesease_Classification.mlflow
+
+export MLFLOW_TRACKING_USERNAME=agshiv92
+
+export MLFLOW_TRACKING_PASSWORD=1d32aaac3b8893b6efd8712ee33e31467dde3aa8
+
+
+$env:MLFLOW_TRACKING_URI = "https://dagshub.com/agshiv92/Chicken_Diesease_Classification.mlflow"
+$env:MLFLOW_TRACKING_USERNAME = "agshiv92"
+$env:MLFLOW_TRACKING_PASSWORD = "1d32aaac3b8893b6efd8712ee33e31467dde3aa8"
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.Chicken_Diesease_Classification import logger
 from src.Chicken_Diesease_Classification.pipeline.stage_01_data_ingestion import  DataIngestionTrainingPipeline
 from src.Chicken_Diesease_Classification.pipeline.stage_02_data_validation import  DataValidationTrainingPipeline
 from src.Chicken_Diesease_Classification.pipeline.stage_03_data_transformation import  DataTransformationTrainingPipeline
+from src.Chicken_Diesease_Classification.pipeline.stage_04_model_trainer import  ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -26,6 +27,16 @@ STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   data_ingestion = ModelTrainerTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
